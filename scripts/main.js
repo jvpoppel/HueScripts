1713ca4ac05c94bafd63cc72a979855cecbec1c2
jvpoppel <johvpoppel@gmail.com>
2018-02-19 00:07:40 +0100

    Account creation

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,14 @@
 var hue = jsHue(); //Instance of Hue
-var bridge = null;
+var bridgeIP = localStorage.getItem("bridgeIP");
+var bridge;
+var username = localStorage.getItem("bridgeUser");
+var user;
+
+var test = false;
 
 
 $(document).ready(function() {
-    getBridges();
+    checkBridge();
 });
 
 function getBridges() {
@@ -15,8 +20,6 @@ function getBridges() {
         if(bridges.length === 0) {
         console.log('No bridges found. :(');
         } else {
-            //bridges.forEach(b => console.log('Bridge found at IP address %s.', b.internalipaddress));
-            //bridges.forEach(b => fndBridges.push(b.internalipaddress));
             for (index = 0; index < bridges.length; ++index) {
                 var b = bridges[index];
                 bSelect.append($('<option>', {
@@ -26,4 +29,57 @@ function getBridges() {
             }
         }
     }).catch(e => console.log('Error finding bridges', e));
+}
+
+function checkBridge() {
+    if ((bridgeIP === null) || (username === null)) {
+        getBridges();
+        $('#ipModal').modal({backdrop: "static"});
+        $('#ipModal').modal('show');
+    } else {
+        bridge = hue.bridge(bridgeIP);
+        user = bridge.user(username);
+    }
+}
+
+function modalSelectIP() {
+
+    /** Fetch, and store bridge IP from dropdown menu */
+    var fetchIP = $('#bridgeSelect :selected').text();
+    console.log(fetchIP);
+    localStorage.setItem("bridgeIP", fetchIP);
+    bridge = hue.bridge(fetchIP);
+
+    $('#ipModal').modal('hide');
+    $('#bridgeModal').modal({backdrop: "static"});
+    $('#bridgeModal').modal('show');
+    // create user account (requires link button to be pressed)
+    bridge.createUser('HueScriptsApp#Account',
+        function (success) {
+            console.log("Success");
+            // extract bridge-generated username from returned data
+            username = data[0].success.username;
+            localStorage.setItem("bridgeUser", username);
+            // instantiate user object with username
+            user = bridge.user(username);
+
+            bridgeConnectSuccess();
+        }, function(error) {
+            console.log(error);
+            $("#discoverText").text("Something went wrong while connecting to your bridge. Refresh and try again.");
+        });
+}
+
+function bridgeConnectSuccess() {
+    $('#bridgeModal').modal('hide');
+    $('#successModal').modal({backdrop: "static"});
+    $('#successModal').modal('show');
+}
+
+function testFnc() {
+    if (test == true) {
+        user.setLightState(1, { bri: 255});
+    } else {
+        user.setLightState(1, { bri: 10});
+    }
 }
