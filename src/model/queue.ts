199850e8666d91813fc3535e2e5a23dfcda2d6dd
Johan van Poppel <johvpoppel@gmail.com>
2020-10-19 13:23:04 +0200

    Begin setup of Mocha and Chai for unit-testing

--- src/model/queue.ts
+++ src/model/queue.ts
@@ -2,7 +2,6 @@ import {TSMap} from "typescript-map";
 import {LightCommand} from "../data/events/lightCommand";
 import {Session} from "../static/session";
 import {Page} from "../data/page";
-import {Light} from "./light";
 
 export class Queue {
     private queue: TSMap<number, Set<LightCommand>>;
@@ -12,6 +11,9 @@ export class Queue {
         this.parse();
     }
 
+    /**
+     * Parse the currently selected page (retrieve from Session class) into an Event Queue.
+     */
     private parse(): void {
         let currentPage: Page = Session.get().currentPage();
         let pageTimes: number[] = currentPage.getSequence().rows().keys();
@@ -26,10 +28,24 @@ export class Queue {
         return;
     }
 
-    private addCommandSetToTime(time: number, commandSet: Set<LightCommand>) {
-        this.queue.sortedSet(time, commandSet);
+    /**
+     * Add a set of commands to given time in the queue
+     * @param time time to add commands to
+     * @param commandSet commands to add to time
+     */
+    private addCommandSetToTime(time: number, commandSet: Set<LightCommand>): void {
+        // If there is already a set mapped to { time }, add all commands to that existing set
+        if (this.queue.keys().indexOf(time) > 0) {
+            commandSet.forEach(lightCommand => this.queue.get(time).add(lightCommand));
+        } else {
+            this.queue.sortedSet(time, commandSet);
+        }
     }
 
+    /**
+     * Return all keys of the queue, sorted ascending.
+     * All keys of the queue correspond with all times on which one, or multiple event(s) happen(s).
+     */
     public eventTimes(): number[] {
         let keys: number[] = this.queue.keys();
         return keys.sort(function (a: number, b: number) {
@@ -37,6 +53,9 @@ export class Queue {
         });
     }
 
+    /**
+     * Return the TSMap object in which all queue data is stored.
+     */
     public map(): TSMap<number, Set<LightCommand>> {
         return this.queue;
     }
