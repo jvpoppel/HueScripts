2151427f4630c38c9df3c0463940f5f4b4ff68f8
Johan van Poppel <johvpoppel@gmail.com>
2020-11-08 12:55:55 +0100

    Test classes for light commands

---
+++ test/data/events/brightnessCommand.test.ts
@@ -0,0 +1,39 @@
+import { expect } from 'chai';
+import {BrightnessCommand} from "../../../src/data/events/brightnessCommand";
+
+describe('BrightnessCommand functionality', () => {
+
+    it('Create command', () => {
+        let command: BrightnessCommand = new BrightnessCommand(1, [255, 255], true)
+        expect(command.light).to.be.equal(1, "Light ID should be 1");
+        expect(command.values[0]).to.be.equal(255, "Brightness should be 255");
+        expect(command.values[1]).to.be.equal(255, "Transition time should be 255");
+        expect(command.executed).to.be.false("Command should not be executed by default");
+    });
+
+    it('Reset a non-executed command', () => {
+        let command: BrightnessCommand = new BrightnessCommand(1, [255, 255], true)
+        expect(command.executed).to.be.false("Command should not be executed by default");
+        command.reset();
+        expect(command.executed).to.be.false("Command should not be executed after reset");
+    });
+
+    it("Execute a command and reset it", () => {
+        let command: BrightnessCommand = new BrightnessCommand(1, [255, 255], true);
+        command.execute();
+        expect(command.executed).to.be.true("Command should be executed");
+        command.reset();
+        expect(command.executed).to.be.false("Command should not be executed after reset");
+    });
+
+    it("Execute a command twice without resetting", () => {
+        let command: BrightnessCommand = new BrightnessCommand(1, [255, 255], true);
+        expect(command.execute()).to.be.true("Command should be successfully executed");
+        expect(command.execute()).to.be.false("Command should not be successfully executed the second time");
+    });
+
+    it("Validation on amount of arguments in values", () => {
+        expect(new BrightnessCommand(1, [])).to.be.an('error', "BrightnessCommand had no values");
+        expect(new BrightnessCommand(1, [1, 2, 3])).to.be.an('error', "BrightnessCommand had too many values");
+    });
+})
